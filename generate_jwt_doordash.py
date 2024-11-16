3effabab797d6f8d716ff2c67dc604c0e96340c1
fahimahmedx <f92ahmed@uwaterloo.ca>
2024-11-16 21:06:54 +0700

    generate JWT token for doordash api

---
+++ generate_jwt_doordash.py
@@ -0,0 +1,28 @@
+from os import access
+import jwt.utils
+import time
+import math
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Access the DOORDASH_SIGNING_SECRET variable
+signing_secret = os.getenv("DOORDASH_SIGNING_SECRET")
+developer_id = os.getenv("DOORDASH_DEVELOPER_ID")
+key_id = os.getenv("DOORDASH_KEY_ID")
+
+token = jwt.encode(
+    {
+        "aud": "doordash",
+        "iss": developer_id,
+        "kid": key_id,
+        "exp": str(math.floor(time.time() + 300)),
+        "iat": str(math.floor(time.time())),
+    },
+    jwt.utils.base64url_decode(signing_secret),
+    algorithm="HS256",
+    headers={"dd-ver": "DD-JWT-V1"})
+
+print(token)
