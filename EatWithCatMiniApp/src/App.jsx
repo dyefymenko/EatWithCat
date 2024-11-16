7c23b3fcf1942d4ce6387ee38de41e9f3df6a9dc
Dasha <dyefymen@uwaterloo.ca>
2024-11-16 12:04:15 +0700

    Update UI of mini app

--- EatWithCatMiniApp/src/App.jsx
+++ EatWithCatMiniApp/src/App.jsx
@@ -1,41 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import "./App.css";
+// React component for the Telegram Mini App UI
+import React from 'react';
+import './App.css';
+
+// Sample data for dishes
+const menuData = [
+    { restaurant: 'The Happy Spoon', dishName: 'Pancake Delight', price: '$8.99', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Sunny Cafe', dishName: 'Avocado Toast', price: '$7.50', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Blue Moon Diner', dishName: 'French Toast Magic', price: '$9.25', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Sparkle Eatery', dishName: 'Berry Bliss Bowl', price: '$10.99', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Golden Fork', dishName: 'Lemon Tart', price: '$6.50', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Crispy Crust', dishName: 'Cheese Pizza', price: '$12.00', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Sweet Dreams', dishName: 'Chocolate Mousse', price: '$5.99', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Healthy Bites', dishName: 'Quinoa Salad', price: '$9.75', photo: 'img/avocado.jpeg' },
+    { restaurant: 'Cozy Corner', dishName: 'Apple Pie', price: '$4.50', photo: 'img/avocado.jpeg' }
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://tokenminds.co" target="_blank">
-          <img
-            src={`https://framerusercontent.com/images/7Fjd4JhBn4XdPoBAztnI31U.webp`}
-            className="logo"
-            alt="Tokenminds logo"
-          />
-        </a>
-      </div>
-      <h1>My First Telegram Mini App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div>
-        <p>
-          Click{" "}
-          <a href="https://docs.ton.org/develop/dapps/telegram-apps/app-examples"
-            target="_blank"
-          >
-            here
-          </a>{" "}
-          to learn more.
-        </p>
-      </div>
-    </>
-  );
+    return (
+        <div id="app">
+            {menuData.map((dish, index) => (
+                <div key={index} className="dish-card">
+                    <div className='dish-photo'>
+                      <img src={dish.photo} alt={dish.dishName} className="dish-photo" height={100} width={100}/>
+                    </div>
+                    <div className="dish-details">
+                        <h3 className="restaurant-name">{dish.restaurant}</h3>
+                        <p className="dish-name">{dish.dishName}</p>
+                        <p className="dish-price">{dish.price}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
