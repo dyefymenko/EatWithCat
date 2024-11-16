233c2f4b1eaa48167ac1cc9b331d0b63365d56a6
Dasha <dyefymen@uwaterloo.ca>
2024-11-17 05:20:34 +0700

    Final image fix

--- EatWithCatMiniApp/src/App.jsx
+++ EatWithCatMiniApp/src/App.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import './App.css';
 
 // Sample data for dishes
-const cats = ['taco.png', 'pasta.png', 'pizza.png', 'melon.png'];
+const cats = ['img/taco.png', 'img/pasta.png', 'img/pizza.png', 'img/melon.png'];
 
 function App() {
     return (
         <div id="app">
             {cats.map((cat, index) => (
                 <div key={index} className="dish-card">
-                    <img src={cat} alt={"kitty with food ${cat}"} />
+                    <img src={cat} alt="kitty with food" />
                 </div>
             ))}
         </div>
